ab0001676e84d1795991421a5f12e84fb627d018
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2018-07-30 14:49:53 +0000

    added widgets and contact form and imported bootstrap and jquery

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    
+    $("#submit").click(function() {
+        $(this).css({"background-color": "#f5bf5c", "border":"solid 1px #f5bf5c" }).text("Sent!");
+    }).mouseenter(function() {
+        $(this).css({"background-color": "#f5bf5c", "transition": "ease-in-out 1s"});
+    }).mouseleave(function() {
+        if($(this).text()=="Submit"){
+            $(this).css("background-color", "#aac4e9");
+        }else{
+            $(this).css("background-color", "#f5bf5c");
+        }
+        });
+})
