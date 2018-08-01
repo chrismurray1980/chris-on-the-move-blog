b4a3a0ee72fcd62780407703346fd4de8242a30d
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2018-08-01 14:51:36 +0000

    removed widgets, added linkedin link, updated css

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     //Contact form submit button user feedback//
     
     $("#submit").click(function() {
-        $(this).css({"background-color": "#f5bf5c", "border":"solid 1px #f5bf5c" }).text("Sent!");
+        $(this).css({ "background-color": "#aac4e9", "border": "solid 2px #f5bf5c"}).text("Sent!");
+        document.forms["contactForm"].reset();
     }).mouseenter(function() {
-        $(this).css({"background-color": "#f5bf5c", "transition": "ease-in-out 1s"});
+        $(this).css({ "background-color": "#aac4e9", "transition": "ease-in-out 1s" });
     }).mouseleave(function() {
-        if($(this).text()=="Submit"){
-            $(this).css("background-color", "#aac4e9");
-        }else{
-            $(this).css("background-color", "#f5bf5c");
+        if ($(this).text() == "Submit") {
+            $(this).css({"background-color": "#f5bf5c"} );
+        } else {
+            $(this).css({"background-color" : "#aac4e9"});
         }
-        });
-        
-   
-})
+    })
+    
+    })
