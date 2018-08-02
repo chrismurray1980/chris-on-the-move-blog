a4a8484e0ad9d17ff3a2d0434bb666b5cd5190df
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2018-08-02 14:48:23 +0000

    added hunners of js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,52 @@
 $(document).ready(function() {
     
+    $("#Previous").mouseenter(function() {
+        $(this).removeClass("blog-section-small").addClass("blog-section");
+        $("#Portfolio").removeClass("blog-section-small").addClass("blog-section");
+        }).mouseleave(function() {
+        $(this).removeClass("blog-section").addClass("blog-section-small");
+        $("#Portfolio").removeClass("blog-section").addClass("blog-section-small").children("p").hide();
+        });
+        
+        $("#Portfolio").mouseenter(function() {
+        $(this).removeClass("blog-section-small").addClass("blog-section");
+        $("#Previous").removeClass("blog-section-small").addClass("blog-section");
+        }).mouseleave(function() {
+        $(this).removeClass("blog-section").addClass("blog-section-small");
+        $("#Previous").removeClass("blog-section").addClass("blog-section-small").children("p").hide();
+        });
+
+    
+    
+    
+    // Previous blog reveal
+    $(".prev-blog-heading").click(function() {
+        $(this).next("p").slideToggle().siblings("p").hide("slow");
+    }).mouseenter(function() {
+        $(this).css("color", "#468ef4");
+    }).mouseleave(function() {
+        $(this).css("color", "#f5bf5c");
+    });
+    
+    // Work done reveal
+    $(".project-heading").click(function() {
+        $(this).next("div").slideToggle().siblings("div").hide("slow");
+        
+        
+        
+        $("#Portfolio").toggleClass("blog-section-small").toggleClass("blog-section");}).mouseenter(function() {
+        $(this).css("color", "#468ef4");
+    }).mouseleave(function() {
+        $(this).css("color", "#f5bf5c");
+    });
+
+
+
+
+
+
+
+
     //Contact form submit button user feedback//
     
     $("#submit").click(function() {
@@ -14,5 +61,12 @@ $(document).ready(function() {
             $(this).css({"background-color" : "#aac4e9"});
         }
     })
+    
+    //Linkedin icon user feedback
+    $("#linkedIn").mouseenter(function() {
+        $(this).css({"opacity" : 0.5 , "transition": "ease-in-out 1s" });
+    }).mouseleave(function() {
+        $(this).css({"opacity" : 1 , "transition": "ease-in-out 1s" });
+    });
     
     })
